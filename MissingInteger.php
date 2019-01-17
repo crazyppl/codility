66550721a002e57fdd2fb102fbdee20b5a7f5449
crazyppl <nadinefirdaus@gmail.com>
2019-01-17 13:14:52 +0600

    Update MissingInteger.php

--- MissingInteger.php
+++ MissingInteger.php
@@ -18,7 +18,7 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [1..100,000];
 each element of array A is an integer within the range [−1,000,000..1,000,000].
 */
-<pre>
+
 function solution($A) {
     // write your code in PHP7.0
     sort($A);
@@ -39,4 +39,3 @@ function solution($A) {
         }
     }
 }
-</pre>
